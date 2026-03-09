3c24c9645c90e4de41eb3998fee39070a5870bed
gu-sean <rnfdc4134@gmail.com>
2026-03-09 23:41:01 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -5,12 +5,16 @@ from datetime import datetime
 a=3
 b=4
 
-#지수승
+# 지수승
 print(a ** b)
 print(a ** 3)
 
-#나머지 연산
+# 나머지 연산
 print(a % b)
 print(7 % 3)
 
-#나눗
+# 나눗셈 몫 구하기
+print(a // b)
+print(7 // 3)
+
+s1=''
