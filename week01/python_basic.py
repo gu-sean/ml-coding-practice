0457f0447057f78aa71a0a86e2c7407990000be6
gu-sean <rnfdc4134@gmail.com>
2026-03-10 00:05:32 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -237,3 +237,11 @@ print(s2.intersection(s3))
 print(s2 | s3)
 print(s2.union(s3))
 
+# 차집합 연산
+print(s2 - s3)
+print(s3 - s2)
+print(s2.difference(s3))
+print(s3.difference(s2))
+
+# 원소 한 개 추가
+s2.add
