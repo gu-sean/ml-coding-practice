fe979338d7a3af5507a4e5a1d763daa6883eb21f
gu-sean <rnfdc4134@gmail.com>
2026-03-26 21:22:49 +0900

    Auto commit: 2026-03-26 21:22 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -89,4 +89,4 @@ print(titanic.info())
 
 # 산점도 그래프 그리기
 plt.figure(figsize=(12,8))
-scatter = plt.scatter(x='Age', y='Fare', data=titanic ,c=titanic['Survived'],)
+scatter = plt.scatter(x='Age', y='Fare', data=titanic ,c=titanic['Survived'],cm)
