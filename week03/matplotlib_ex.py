8f1c745706f3ce957efddbf6d7e37b22535b9bff
gu-sean <rnfdc4134@gmail.com>
2026-03-26 21:33:40 +0900

    Auto commit: 2026-03-26 21:33 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -140,4 +140,4 @@ plt.close()
 titanic = titanic.dropna(subset=['Age', 'Fare'])
 
 # 상관 행렬 계산
-correlation_matrix = titanic.drop('PassengerId', axis=1).corr(nu)
+correlation_matrix = titanic.drop('PassengerId', axis=1).corr(numeric_only=True)
