fafb12fb32152e537c44b895cba25d98e586d5f5
gu-sean <rnfdc4134@gmail.com>
2026-03-26 21:52:01 +0900

    Auto commit: 2026-03-26 21:52 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -230,3 +230,5 @@ plt.xticks([1, 2, 3], ['1st Class', '2nd Class', '3rd Class'])
 # 범례 설정
 plt.legend(violin_plotp['bodies'], ['1st Class', '2nd Class', '3rd Class'],
            title='Pclass', loc="upper right")
+plt.savefig('Figure10.png')
+plt.close()
