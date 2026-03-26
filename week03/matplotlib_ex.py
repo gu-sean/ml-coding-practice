15e79921ae759c844bb4d1c5bae54cca6506c64a
gu-sean <rnfdc4134@gmail.com>
2026-03-26 21:27:01 +0900

    Auto commit: 2026-03-26 21:27 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -107,4 +107,5 @@ print(survived_counts)
 
 # 파이 차트 그리기
 plt.figure(figsize=(8,8))
-plt.pie(survived_counts, labels=['Not Survived', 'Survived'], colors=['orange', 'gold'])
+plt.pie(survived_counts, labels=['Not Survived', 'Survived'], colors=['orange', 'gold'],
+        )
