bb129fc1203a414b86425054fec09a3f526c04b5
gu-sean <rnfdc4134@gmail.com>
2026-03-26 21:28:26 +0900

    Auto commit: 2026-03-26 21:28 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -112,4 +112,4 @@ plt.pie(survived_counts, labels=['Not Survived', 'Survived'], colors=['orange', 
 
 plt.title('Survival Distribution on the Titanic')
 plt.savefig('Figure05.png')
-plt.cl
+plt.close()
