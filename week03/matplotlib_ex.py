3113138f07d19ba3d2700625da4ebb852c309053
gu-sean <rnfdc4134@gmail.com>
2026-03-26 21:13:32 +0900

    Auto commit: 2026-03-26 21:13 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -48,3 +48,5 @@ plt.ylabel('Count')
 plt.xticks(survived_counts.index, ['Southampton', 'Cherbourg', 'Queenstown'])
 plt.legend(['Survived'], loc='upper right')
 plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+# 생존자 수 표시
