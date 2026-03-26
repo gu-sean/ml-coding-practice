6deeafe1c89ec586770dbdd1680a0284fd4f8621
gu-sean <rnfdc4134@gmail.com>
2026-03-26 22:08:19 +0900

    Auto commit: 2026-03-26 22:08 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -311,4 +311,11 @@ plt.legend(loc='upper right')
 
 # 제목 설정
 plt.suptitle('Survival Analysis by Number of Parents/Children (Parch) on the Titabic')
-plt.ti
+plt.tight_layout()
+plt.savefig('Figure13.png')
+plt.close()
+
+## **개별 서브플롯을 동시에 생성하기**
+
+# 2*2 구성의 figure
+fig, axes = 
