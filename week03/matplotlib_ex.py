c73135af2c5b4134a2ad5e1310dde43f28c51123
gu-sean <rnfdc4134@gmail.com>
2026-03-26 21:11:21 +0900

    Auto commit: 2026-03-26 21:11 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -43,3 +43,4 @@ print(survived_counts)
 plt.bar(survived_counts.index, survived_counts,
         color = ['mediumorchid','darkviolet','indigo'])
 plt.title('Survived Counts by Embarked Port on Titanic')
+plt.
