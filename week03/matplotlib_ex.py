2dad0583033d9291cc7775c786801baf55f4fb93
gu-sean <rnfdc4134@gmail.com>
2026-03-26 21:24:23 +0900

    Auto commit: 2026-03-26 21:24 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -94,4 +94,5 @@ scatter = plt.scatter(x='Age', y='Fare', data=titanic ,c=titanic['Survived'],cma
 plt.title('Age and Fare Relationship with Survival on the Titanic')
 plt.xlabel('Age')
 plt.ylabel('Fare')
-plt.legend(handles=scatter.legend_elements()[0], title='Survived')
+plt.legend(handles=scatter.legend_elements()[0], title='Survived',
+           )
