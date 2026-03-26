3e88c4a1fad62901feb0dea382434edb4c5340c5
gu-sean <rnfdc4134@gmail.com>
2026-03-26 21:13:45 +0900

    Auto commit: 2026-03-26 21:13 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -50,4 +50,5 @@ plt.legend(['Survived'], loc='upper right')
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 
 # 생존자 수 표시
-for i, value in enumerate(survived_counts)
+for i, value in enumerate(survived_counts):
+    
