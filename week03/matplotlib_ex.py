7cdb88e3dccb4274e7b9f7b96cbe4123e694bc83
gu-sean <rnfdc4134@gmail.com>
2026-03-26 22:41:24 +0900

    Auto commit: 2026-03-26 22:41 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -215,9 +215,9 @@ print(titanic.info())
 plt.figure(figsize=(10,6))
 
 # showmeans=False는 평균값을 표시하지 않도록 하고, showmedians=True는 중앙값을 표시하도록 함
-violin_plot = plt.violinplot([titanic[titanic['Pcalss'] == 1]['Age'],
-                            titanic[titanic['Pcalss'] == 2]['Age'],
-                            titanic[titanic['Pcalss'] == 3]['Age']],
+violin_plot = plt.violinplot([titanic[titanic['Pclass'] == 1]['Age'],
+                            titanic[titanic['Pclass'] == 2]['Age'],
+                            titanic[titanic['Pclass'] == 3]['Age']],
                             showmeans=False, showmedians=True)
 
 plt.title('Violin Plot of Age by Pclass')
