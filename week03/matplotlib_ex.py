0265ec74384f9f00a25d70dc98832e1ce39704fb
gu-sean <rnfdc4134@gmail.com>
2026-03-26 21:34:10 +0900

    Auto commit: 2026-03-26 21:34 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -144,4 +144,4 @@ correlation_matrix = titanic.drop('PassengerId', axis=1).corr(numeric_only=True)
 print(correlation_matrix)
 
 # 히트맵 그리기
-plt.matshow(correlation_matrix, c)
+plt.matshow(correlation_matrix, cmap)
