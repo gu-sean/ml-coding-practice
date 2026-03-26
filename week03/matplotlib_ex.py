43f792cba59772179fc9f0d4b29d97e832c75554
gu-sean <rnfdc4134@gmail.com>
2026-03-26 21:46:09 +0900

    Auto commit: 2026-03-26 21:46 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -199,4 +199,5 @@ plt.boxplot([titanic[titanic['Pcalss'] == 1]['Age'],
              titanic[titanic['Pcalss'] == 3]['Age']],
              label=['1st Class', '2nd Class', '3rd Class'])
 
-plt.title()
+plt.title('Box Plot for Age by Pclass')
+plt.x
