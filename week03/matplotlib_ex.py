b2066b054178a9339d42ab6fb5b0675088d41627
gu-sean <rnfdc4134@gmail.com>
2026-03-26 21:17:47 +0900

    Auto commit: 2026-03-26 21:17 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -65,4 +65,8 @@ print(survived_counts)
 # 수평 막대 그래프 그리기
 bars = plt.barh(survived_counts.index, survived_counts, color=['darkturquoise', 'salmon'])
 plt.title('Survived Counts by Gender on Titanic')
-plt.xlabel()
+plt.xlabel('Count')
+plt.ylabel('Gender')
+plt.legend(bars, [
+
+])
