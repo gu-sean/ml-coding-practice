179d5f2544ff80314e9c0371e30834f1b716b679
gu-sean <rnfdc4134@gmail.com>
2026-03-26 21:36:10 +0900

    Auto commit: 2026-03-26 21:36 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -151,4 +151,4 @@ plt.colorbar()
 plt.xticks(range(len(correlation_matrix.columns)), correlation_matrix.columns, rotation=45)
 plt.yticks(range(len(correlation_matrix.columns)), correlation_matrix.columns)
 
-plt.title('Corr')
+plt.title('Correlation Heat')
