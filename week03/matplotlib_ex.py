a346b9a529a6458c8f67c372f7f984f5fe8b7ba7
gu-sean <rnfdc4134@gmail.com>
2026-03-26 22:12:24 +0900

    Auto commit: 2026-03-26 22:12 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -342,4 +342,5 @@ y2 = parch_counts[1].values # 생존자
 # plt.subplots() 함수를 사용하여 하나의 그림인  figure에 개별 서브플롯을 동시에 추가
 fig, axes = plt.subplot(2, 1, figsize=(10,10))
 
-# 
+# 첫 번째 서브플롯 설정(선 그래프)
+axes[0]
