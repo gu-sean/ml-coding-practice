335d348455d37cc2874744ab76876cf3abec9497
gu-sean <rnfdc4134@gmail.com>
2026-03-26 21:20:24 +0900

    Auto commit: 2026-03-26 21:20 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -76,4 +76,5 @@ plt.axvline(x=survived_counts['male'], color='gray', linestyle='--', linewidth=1
 for i, value in enumerate(survived_counts):
     plt.text(value + 1, i, str(value), ha='left', va='center')
     
-plt.savefig('Figur')
+plt.savefig('Figure03.png')
+plt.close()
