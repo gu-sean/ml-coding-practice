8a46cac678349987a3451d4bc68bbaafc63648b3
gu-sean <rnfdc4134@gmail.com>
2026-03-31 23:55:13 +0900

    Auto commit: 2026-03-31 23:55 - 저장됨

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -88,4 +88,4 @@ from sklearn.preprocessing import OrdinalEncoder
 ordinal_encoder = OrdinalEncoder()
 housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
 
-ho
+housing_cat_encoded[:8]
