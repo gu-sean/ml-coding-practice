d34da6283831764dda36f0355116bdfe07bc0512
gu-sean <rnfdc4134@gmail.com>
2026-03-31 23:48:32 +0900

    Auto commit: 2026-03-31 23:48 - 저장됨

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -46,4 +46,6 @@ housing_labels = strat_train_set["median_house_value"].copy()
 null_rows_idx = housing.isnull().any(axis=1)
 housing.loc[null_rows_idx].head()
 
-from sklearn.im
+from sklearn.impute import SimpleImputer
+
+impu
