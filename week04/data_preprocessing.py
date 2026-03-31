a7ae8d106df44c3178d5c349406b499615021f58
gu-sean <rnfdc4134@gmail.com>
2026-03-31 23:42:42 +0900

    Auto commit: 2026-03-31 23:42 - 저장됨

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -24,5 +24,5 @@ from sklearn.model_selection import train_test_split
 
 housing["income_cat"] = pd.cut(housing["median_income"], 
                                bins=[0., 1.5, 3.0, 4.5, 6., np.inf], 
-                               labels=[1,2,3,4,5])
+                            labels=[1,2,3,4,5])
 
