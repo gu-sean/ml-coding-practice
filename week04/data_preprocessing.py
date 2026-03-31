5a22d590903d220c4d0de500020e360a2b0efbb3
gu-sean <rnfdc4134@gmail.com>
2026-03-31 23:44:31 +0900

    Auto commit: 2026-03-31 23:44 - 저장됨

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -30,4 +30,4 @@ strat_train_set, strat_tast_set = train_test_split(
     housing, test_size=0.2, stratify=housing["income_cat"], random_state=42)
 
 for set_ in (strat_train_set, strat_tast_set):
-    set_.drop("income_cat", axis=1, inp)
+    set_.drop("income_cat", axis=1, inplace=True)
