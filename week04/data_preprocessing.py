12a0015b2cc87dd89a54ae291aa43d7824f45582
gu-sean <rnfdc4134@gmail.com>
2026-03-31 23:46:28 +0900

    Auto commit: 2026-03-31 23:46 - 저장됨

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -36,5 +36,5 @@ for set_ in (strat_train_set, strat_tast_set):
 * 원본 훈련 세트로 복원하고 타깃을 분리
 * 'strat_train_set.drop()'은 지정한 열을 제외한 'strat_train_srt'의 복사복을 만듦
 * 'inplace=True'로 지정하지 않은 한 'strat_train_set' 자체를 수정하지 않음
-    """
+"""
     
