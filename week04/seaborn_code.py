39f900b12a16253ae491257d396fd5e3df4a56ba
gu-sean <rnfdc4134@gmail.com>
2026-04-01 00:09:56 +0900

    Auto commit: 2026-04-01 00:09 - 저장됨

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -62,3 +62,4 @@ sns.regplot(x='total_bill', y='tip', data=tips, color='blue', scatter_kws={'s':5
 fig.suptitle('Scatter Plots with Regression Lines', fontsize=16)
 ax1.set_title('fit_reg = True')
 ax2.set_title('fit_reg = False')
+plt.s
