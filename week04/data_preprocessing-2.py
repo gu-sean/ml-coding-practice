9fa996d762732fa1332546358bed1aeabc9e92c8
gu-sean <rnfdc4134@gmail.com>
2026-04-02 21:16:30 +0900

    Auto commit: 2026-04-02 21:16 - 저장됨

--- week04/data_preprocessing-2.py
+++ week04/data_preprocessing-2.py
@@ -51,4 +51,5 @@ X = imputer.transform(housing_num)
 
 imputer.feature_names_in_
 
-housing_tr = pd.DataFrame(X, columns=housing_num.columns,)
+housing_tr = pd.DataFrame(X, columns=housing_num.columns,
+                        )
