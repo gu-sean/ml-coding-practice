098f679bb9711bb42fa72f0e756e5f8666ca22c3
gu-sean <rnfdc4134@gmail.com>
2026-04-02 21:17:32 +0900

    Auto commit: 2026-04-02 21:17 - 저장됨

--- week04/data_preprocessing-2.py
+++ week04/data_preprocessing-2.py
@@ -58,4 +58,4 @@ housing_tr.loc[null_rows_idx].head()
 # 이상치 삭제
 from sklearn.ensemble import IsolationForest
 
-isolation_forest = IsolationForest(ran)
+isolation_forest = IsolationForest(random_state=42)
