5ce67a74a37dc668e22b78619994b5007998f487
gu-sean <rnfdc4134@gmail.com>
2026-04-02 21:19:29 +0900

    Auto commit: 2026-04-02 21:19 - 저장됨

--- week04/data_preprocessing-2.py
+++ week04/data_preprocessing-2.py
@@ -73,4 +73,4 @@ housing_cat.head(8)
 from sklearn.preprocessing import OrdinalEncoder
 
 ordinal_encoder = OrdinalEncoder()
-housing_cat_encoded = ordinal_encoder.fit_transform()
+housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
