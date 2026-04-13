4863a99691fd5e1b3dd61ebf1ea25485eed4ec6f
gu-sean <rnfdc4134@gmail.com>
2026-04-13 16:06:03 +0900

    Auto commit: 2026-04-13 16:06 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -61,5 +61,6 @@ def getRequestUrl(url):
         response = urllib.request.urlopen(req)
         if response.getcode() == 200:
             print("[%s] Url Request Success" % datetime.datetime.now())
-            return response.read().decode('')
+            return response.read().decode('utf-8')
+        
     
