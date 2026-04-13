e348cf8ad644fc95a879f7d5ef0459af0d9b8f12
gu-sean <rnfdc4134@gmail.com>
2026-04-13 15:34:40 +0900

    Auto commit: 2026-04-13 15:34 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -21,4 +21,4 @@ def main():
     jsonResult, result, natName, dataEND = getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear)  #[CODE 3]
     
     #파일저장 : csv 파일
-    colums
+    columns = ["입국자국가", ""]
