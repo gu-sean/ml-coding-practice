99fb6871db7fc9bb4474ad97a9c9c085a2bf7cf6
gu-sean <rnfdc4134@gmail.com>
2026-04-13 16:46:22 +0900

    Auto commit: 2026-04-13 16:46 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -36,7 +36,7 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
             yyyymm = "{0}{1:0>2}".format(str(year), str(month))
             jsonData = getTourismStatsItem(yyyymm, nat_cd, ed_cd)  #[CODE 2]
             if (jsonData['response']['header']['resultMsg'] == 'OK'):
-                    #데이터가 없는 마지막 항목인 경우 -----------------------
+                #데이터가 없는 마지막 항목인 경우 -----------------------
                     if jsonData['response']['body']['items'] == '':
                         dataEND = "{0}{1:0>2}".format(str(year), str(month-1))
                         print("데이터 없음.... \n제공되는 통계 데이터는 %s년 %s월까지입니다." % (str(year), str(month-1)))
