9bc5d37158521c3349a0ad85f2ab72d4baebc0af
gu-sean <rnfdc4134@gmail.com>
2026-04-13 15:43:02 +0900

    Auto commit: 2026-04-13 15:43 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -43,3 +43,4 @@ def main():
                         break
                     #jsonData를 출력하여 확인................................
                     print(json.dumps(jsonData, indent = 4, sort_keys = True,ensure_ascii = False))
+                    
