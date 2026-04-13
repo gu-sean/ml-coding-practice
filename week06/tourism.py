4471921ddda98b4455b5bed8ee798f816cc8631d
gu-sean <rnfdc4134@gmail.com>
2026-04-13 16:40:00 +0900

    Auto commit: 2026-04-13 16:39 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -53,7 +53,7 @@ def main():
                     jsonResult.append({'nat_name' : natName, 'nat_cd' : nat_cd, 'yyyymm' : yyyymm, 'visit_cnt': num})
                     result.append([natName, nat_cd, yyyymm, num])
                     
-    result (jsonResult, result, natName, ed)
+    return (jsonResult, result, natName, ed)
     
 """### [CODE 2]"""
 
