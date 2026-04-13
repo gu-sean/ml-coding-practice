3de6b648fda05f4a337764630552727de95c0667
gu-sean <rnfdc4134@gmail.com>
2026-04-13 15:42:00 +0900

    Auto commit: 2026-04-13 15:42 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -40,3 +40,4 @@ def main():
                     if jsonData['response']['body']['items'] == '':
                         dataEND = "{0}{1:0>2}".format(str(year), str(month-1))
                         print("데이터 없음.... \n제공되는 통계 데이터는 %s년 %s월까지입니다." % (str(year), str(month-1)))
+                        
