6917ee9232d80b4d07bc842c1cc9b5b045ad11c5
gu-sean <rnfdc4134@gmail.com>
2026-04-13 16:46:05 +0900

    Auto commit: 2026-04-13 16:46 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -28,7 +28,7 @@ def main():
     """### [CODE 3]"""
     
 def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
-        jsonResult = []
+    jsonResult = []
         result = []
         
         for year in range(nStartYear, nEndYear+1):
