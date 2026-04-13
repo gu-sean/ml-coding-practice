89087a0a35000bfbc735eb21b195891b5ed1df6a
gu-sean <rnfdc4134@gmail.com>
2026-04-13 15:57:59 +0900

    Auto commit: 2026-04-13 15:57 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -22,4 +22,4 @@ def main():
             cnt += 1
             getPostData(post, jsonResult, cnt)  #[CODE 3]
             
-        start = jsonResult['start'] + jsonResult
+        start = jsonResult['start'] + jsonResult['dispal']
