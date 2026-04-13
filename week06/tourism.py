ecdfecd917655a906bbadec03f64c6eb741c920c
gu-sean <rnfdc4134@gmail.com>
2026-04-13 15:45:03 +0900

    Auto commit: 2026-04-13 15:45 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -48,3 +48,4 @@ def main():
                     natName = natName.replace(' ','')
                     num = jsonData['response']['body']['items']['item']['num']
                     ed = jsonData['response']['body']['items']['item']['ed']
+                    print('[]')
