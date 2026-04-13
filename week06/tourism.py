0a6bbd915e5f6bab9f045d772820a0e661cc1d22
gu-sean <rnfdc4134@gmail.com>
2026-04-13 15:47:56 +0900

    Auto commit: 2026-04-13 15:47 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -59,3 +59,4 @@ def main():
 
 def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
     service_url = "http://openapi.tour.go.kr/openapi/service/EdrcntTourismStatsService/getEdrcntTourismStatsList"
+    
