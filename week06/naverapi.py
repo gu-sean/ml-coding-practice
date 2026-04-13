1d6a4c697f2aa57acf78087789de17a84db77104
gu-sean <rnfdc4134@gmail.com>
2026-04-13 16:10:41 +0900

    Auto commit: 2026-04-13 16:10 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -80,5 +80,5 @@ def getPostData(post, jsonResult, cnt): #[CODE 3]
     pDate = datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S +0900')
     pDate = pDate.strftime('%Y-%m-%d %H:%M:%S')
     
-    jsonResult.append
+    jsonResult.append({})
     
