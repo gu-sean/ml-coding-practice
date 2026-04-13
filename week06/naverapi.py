ae2d2accc7b7c3a3830c466c29fea4d476bbcbd7
gu-sean <rnfdc4134@gmail.com>
2026-04-13 16:04:49 +0900

    Auto commit: 2026-04-13 16:04 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -55,5 +55,5 @@ def getRequestUrl(url):
     req = urllib.request.Request(url)
     
     req.add_header("X-Naver-Client-Id", client_id)
-    req.add_header("X-Naver-Client-Ser")
+    req.add_header("X-Naver-Client-Secret")
     
