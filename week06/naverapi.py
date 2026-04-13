a8b2a633c6b794c75908f77ccbe48a5201f218ee
gu-sean <rnfdc4134@gmail.com>
2026-04-13 16:07:51 +0900

    Auto commit: 2026-04-13 16:07 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -72,5 +72,5 @@ def getPostData(post, jsonResult, cnt): #[CODE 3]
     title = post['title']
     description = post['description']
     org_link = post['originallink']
-    link = 
+    link = post['link']
     
