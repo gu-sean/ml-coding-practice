c54e6ed5b81b55c816eee737c9ad8e6f705e7357
gu-sean <rnfdc4134@gmail.com>
2026-04-13 15:39:41 +0900

    Auto commit: 2026-04-13 15:39 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -35,4 +35,4 @@ def main():
             for month in range(1, 13):
                 yyyymm = "{0}{1:0>2}".format(str(year), str(month))
                 jsonData = getTourismStatsItem(yyyymm, nat_cd, ed_cd)  #[CODE 2]
-                if (jsonData['response']['header']['resultMsg'] == '')
+                if (jsonData['response']['header']['resultMsg'] == 'OK')
