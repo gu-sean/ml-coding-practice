26a69d7c887134442345c7eceff5a134b90cf6f1
gu-sean <rnfdc4134@gmail.com>
2026-04-30 00:08:15 +0900

    Auto commit: 2026-04-30 00:08 - 저장됨

--- week07/classification.py
+++ week07/classification.py
@@ -91,4 +91,4 @@ y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3, method="decision
 roc_auc_score(y_train_5, y_scores)
 
 #다음은 실행하는데 몇 분 걸릴 수 있음
-from sklearn.metrics import RandomF
+from sklearn.metrics import RandomForestClassifier
