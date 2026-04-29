fc712859749a4e14e0296f9348f155fdf8128151
gu-sean <rnfdc4134@gmail.com>
2026-04-29 23:53:21 +0900

    Auto commit: 2026-04-29 23:53 - 저장됨

--- week07/classification.py
+++ week07/classification.py
@@ -16,4 +16,5 @@ import matplotlib.pyplot as plt
 def plot_digit(image_data):
     image = image_data.reshape(28, 28)
     plt.imshow(image, cmap="binary")
-    plt.axis()
+    plt.axis("off")
+    
