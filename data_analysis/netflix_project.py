9c6516e8dda5f270566882b174c6a9e1c6d6dfba
gu-sean <rnfdc4134@gmail.com>
2026-05-04 17:39:34 +0900

    Auto commit: 2026-05-04 17:39 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -132,4 +132,4 @@ genres
 
 plt.figure(figsize=(12, 6))
 
-sns.barplot(x=genres.values, y=genres.index, hue=genres. index, pale)
+sns.barplot(x=genres.values, y=genres.index, hue=genres. index, palette = )
