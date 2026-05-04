f5ac96047576b323adbdcf5e21e5e12aa8f3ee4b
gu-sean <rnfdc4134@gmail.com>
2026-05-04 17:54:35 +0900

    Auto commit: 2026-05-04 17:54 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -171,3 +171,4 @@ country_order = ['United States', 'India', 'United Kingdom', 'Canada', 'Japan',
 netflix_age_country_unstack = netflix_age_country.unstack.loc[age_order, country_order]
 
 # 결측치 0으로 처리
+netflix
