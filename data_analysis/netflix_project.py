5a7d785f53ea7f248ada20a34f4df11ccfc1b8d4
gu-sean <rnfdc4134@gmail.com>
2026-05-04 17:29:50 +0900

    Auto commit: 2026-05-04 17:29 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -105,4 +105,4 @@ print(type_count)
 plt.figure(figsize=(5, 5))
 
 plt.pie(type_count, labels=type_count.index, autopct='%0.f%%', startangle=100,
-        explode=[0.05, 0.05], shadow=True, colors=['#'])
+        explode=[0.05, 0.05], shadow=True, colors=['#b20'])
