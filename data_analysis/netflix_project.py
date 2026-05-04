3c49c7b63054492b6fe3313d733671c4235afc38
gu-sean <rnfdc4134@gmail.com>
2026-05-04 17:43:38 +0900

    Auto commit: 2026-05-04 17:43 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -148,3 +148,5 @@ pd.set_option('display.max_rows', None)
 # 쉼표로 country 열의 값을 파이썬 리스트로 만들기
 netflix['country'] = netflix['country'].str.split(', ')
 netflix['country']
+
+# 파이썬 리스트로 바꾼 country 열의 값에 
