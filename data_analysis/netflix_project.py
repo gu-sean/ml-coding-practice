2660a95b8d5db941c8b4a987f2f9d55aaf991626
gu-sean <rnfdc4134@gmail.com>
2026-05-04 18:11:57 +0900

    Auto commit: 2026-05-04 18:11 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -87,7 +87,7 @@ import seaborn as sns
 netflix = pd.read_csv('netflix_preprocessed.csv')
 
 # 넷플릭스 브랜드 상진 색깔 시간화
-sns.palplot(['#221f1f','#b20710','#e50914','#f5f5f1')
+sns.palplot(['#221f1f','#b20710','#e50914','#f5f5f1'])
 
 # 제목 정하기
 plt.title('Netflix brand palette', loc='left', fontfamily='serif',fontsize=15, y=1.2)
