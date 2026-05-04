ca27dd34f69e6c8640f4fb2e3f2d57fe323f45bc
gu-sean <rnfdc4134@gmail.com>
2026-05-04 18:01:46 +0900

    Auto commit: 2026-05-04 18:01 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -188,4 +188,4 @@ sns.heatmap(netflix_age_country_unstack, cmap = cmap, linewidths=2.5, annot=True
 plt.suptitle('Target ages proportion of total content by country', 
             fontweight='bold', fontfamily='serif', fontsize=15)
 plt.title('Here we see interesting differences between countries. Most shows in South Korea are targeted to adults, for instance.',
-        font)
+        fontsize)
