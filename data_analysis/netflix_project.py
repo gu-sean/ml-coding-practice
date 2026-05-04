9ac7371b178c5318fec246d2fa7c23510c05e1d2
gu-sean <rnfdc4134@gmail.com>
2026-05-04 17:37:12 +0900

    Auto commit: 2026-05-04 17:37 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -125,4 +125,4 @@ netflix['listed_in'].str.split(',', expand=True)
 # 예시) 인덱스 1의 listed_in 열 값 : International TV Shows, TV Dramas, TV Mysteries
 netflix['listed_in'].str.split(',', expand=True).stack()
 
-# [3단계] 
+# [3단계] .value_count()
