ab0a8f8369b1227a0d9460ff1c210703664e0a7b
gu-sean <rnfdc4134@gmail.com>
2026-05-04 18:02:33 +0900

    Auto commit: 2026-05-04 18:02 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -191,4 +191,6 @@ plt.title('Here we see interesting differences between countries. Most shows in 
         fontsize=12, fontfamily='serif')
 plt.show()
 
-# 넷플릭스 데이터의 descr
+# 넷플릭스 데이터의 description 열 이용한 워드 클라우드 생성
+
+# Word
