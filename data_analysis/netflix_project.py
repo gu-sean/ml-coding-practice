47f595c0f1bca84fc9f81fd649950ce3b916312d
gu-sean <rnfdc4134@gmail.com>
2026-05-04 17:32:30 +0900

    Auto commit: 2026-05-04 17:32 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -108,4 +108,4 @@ plt.pie(type_count, labels=type_counts.index, autopct='%0.f%%', startangle=100,
         explode=[0.05, 0.05], shadow=True, colors=['#b20710', '#221f1f'])
 
 plt.suptitle('Movie & TV Show distribution', fontfamily='serif', fontsiaze=15, fontweight='bold')
-plt.title('We see more movies than TV shows on Netfilx')
+plt.title('We see more movies than TV shows on Netfilx.',)
