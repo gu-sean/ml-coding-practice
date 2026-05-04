df817fe01db20b6c9ce46c5aa914b8ba8ea3a8ce
gu-sean <rnfdc4134@gmail.com>
2026-05-04 17:11:42 +0900

    Auto commit: 2026-05-04 17:11 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -36,4 +36,4 @@ netflix['cast'] = netflix['cast'].replace(np.nan, 'No Data')
 
 # .dropna(axis = 0) : 결측치가 있는 행 전체 제거
 # 원본 객체를 수정하려면 inplace - True 옵션 추가
-# 결측치 비율 : data_added(0.11%), rating(0.05%), durati
+# 결측치 비율 : data_added(0.11%), rating(0.05%), duration(0.03%)
