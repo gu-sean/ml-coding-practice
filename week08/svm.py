cc704d557384c10dcdab14096537e0890b547861
gu-sean <rnfdc4134@gmail.com>
2026-05-08 21:27:35 +0900

    Auto commit: 2026-05-08 21:27 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -32,4 +32,7 @@ polynomial_svm_clf = make_pipeline(
     StandardScaler(),
     LinearSVC(C=10, max_iter=10_000, dual=True, random_state=42)
 )
-polynomial_svm_clf.
+polynomial_svm_clf.fit(X, y)
+
+# 다항식 커널
+
