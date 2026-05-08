b7b67617b4e684047d623a8f5e7dda8bce3e8522
gu-sean <rnfdc4134@gmail.com>
2026-05-08 21:19:55 +0900

    Auto commit: 2026-05-08 21:19 - 저장됨

--- week08/regression.py
+++ week08/regression.py
@@ -154,4 +154,4 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 log_reg = LogisticRegression(random_state=42)
 log_reg.fit(X_train, y_train)
 
-print(log_reg)
+print(log_reg.)
