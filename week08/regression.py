5283c0f60cc2adee3106475a504667b51861229e
gu-sean <rnfdc4134@gmail.com>
2026-05-08 21:18:06 +0900

    Auto commit: 2026-05-08 21:18 - 저장됨

--- week08/regression.py
+++ week08/regression.py
@@ -145,4 +145,4 @@ print(iris.target.head(3))   # 샘플이 섞여 있지 않습니다.
 print(iris.target_names)
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import 
+from sklearn.model_selection import train_test_split
