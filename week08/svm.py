b237e1d6c057bd1dd555b6e88fc04c60e3a2b624
gu-sean <rnfdc4134@gmail.com>
2026-05-08 21:28:29 +0900

    Auto commit: 2026-05-08 21:28 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -39,4 +39,5 @@ polynomial_svm_clf.fit(X, y)
 from sklearn.svm import SVC
 
 poly_kernel_svm_clf = make_pipeline(StandardScaler(),
-                                    SVC(kernel="poly", degree=3))
+                                    SVC(kernel="poly", degree=3),
+                                    )
