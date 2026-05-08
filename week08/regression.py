73bd60881317ad7ff71135906ee2a6a58bc1d5b4
gu-sean <rnfdc4134@gmail.com>
2026-05-08 21:19:40 +0900

    Auto commit: 2026-05-08 21:19 - 저장됨

--- week08/regression.py
+++ week08/regression.py
@@ -152,4 +152,4 @@ y = iris.target_names[iris.target] == 'virginica'
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
 log_reg = LogisticRegression(random_state=42)
-l
+log_reg.
