4fd8c8e1716e7178994fc0bbc96fa6be87e69879
gu-sean <rnfdc4134@gmail.com>
2026-05-08 21:19:26 +0900

    Auto commit: 2026-05-08 21:19 - 저장됨

--- week08/regression.py
+++ week08/regression.py
@@ -151,4 +151,4 @@ X = iris.data[["petal width (cm)"]].values
 y = iris.target_names[iris.target] == 'virginica'
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
-log_
+log_reg = 
