5f762d79f0f1bfc77c2eacb68d14ad04d9345ab0
gu-sean <rnfdc4134@gmail.com>
2026-05-08 21:28:49 +0900

    Auto commit: 2026-05-08 21:28 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -41,4 +41,3 @@ from sklearn.svm import SVC
 poly_kernel_svm_clf = make_pipeline(StandardScaler(),
                                     SVC(kernel="poly", degree=3),
                                     coef0=1, C=5)
-
