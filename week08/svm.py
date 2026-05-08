4f63a3b7c517e2ce2bdd453bd295534d26e0b13d
gu-sean <rnfdc4134@gmail.com>
2026-05-08 21:30:19 +0900

    Auto commit: 2026-05-08 21:30 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -46,3 +46,7 @@ polynomial_svm_clf.fit(X, y)
 rbf_kernel_svm_clf = make_pipeline(StandardScaler(),
                                 SVC(kernel="rbf", gamma=5, C=0.001))
 rbf_kernel_svm_clf.fit(X, y)
+
+# SVM 회귀
+
+from sk
