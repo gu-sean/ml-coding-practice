245fe772c3e85c91891a74376ebcb61ce63c98e2
gu-sean <rnfdc4134@gmail.com>
2026-05-08 21:29:45 +0900

    Auto commit: 2026-05-08 21:29 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -43,4 +43,5 @@ poly_kernel_svm_clf = make_pipeline(StandardScaler(),
 polynomial_svm_clf.fit(X, y)
 
 # 가우스 RBF 커널
-rbf_kernel_svm_clf = make_pipeline(StandardScaler(),)
+rbf_kernel_svm_clf = make_pipeline(StandardScaler(),
+                                )
