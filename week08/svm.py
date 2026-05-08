6b4775f221b1073a63867c3fd478ef30bbe64246
gu-sean <rnfdc4134@gmail.com>
2026-05-08 21:24:12 +0900

    Auto commit: 2026-05-08 21:24 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -8,4 +8,4 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
 
 iris = load_iris(as_frame=True)
-X = iris.data[["petal length (cm)", "petal width (cm)"]].valu
+X = iris.data[["petal length (cm)", "petal width (cm)"]].values
