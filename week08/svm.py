31e01074673f12ada0181824b69ebcbb5b1b0abe
gu-sean <rnfdc4134@gmail.com>
2026-05-08 21:34:16 +0900

    Auto commit: 2026-05-08 21:34 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -68,4 +68,4 @@ X = 2 * np.random.rand(50, 1) - 1
 y = 0.2 + 0.1 * X[:, 0] + 0.5 * X[:, 0] ** 2 + np.random.randn(50) / 10
 
 svm_poly_reg = make_pipeline(StandardScaler(),
-                            SVR(kernel=))
+                            SVR(kernel="poly", degree=2, ))
