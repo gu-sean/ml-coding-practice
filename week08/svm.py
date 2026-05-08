aa2a36b8b889d0301acbc97993f87d30ab366673
gu-sean <rnfdc4134@gmail.com>
2026-05-08 21:31:41 +0900

    Auto commit: 2026-05-08 21:31 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -56,4 +56,5 @@ np.random.seed(42)
 X = 2 * np.random.rand(50, 1)
 y = 4 + 3 * X[:, 0] + np.random.randn(50)
 
-svm_reg = make_pipeline(StandardScaler(),)
+svm_reg = make_pipeline(StandardScaler(),
+                        )
