6ad36702e0d581783c13842e19d01bd76abc5907
gu-sean <rnfdc4134@gmail.com>
2026-05-28 23:44:41 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -8,3 +8,7 @@ plt.rc('font', size=14)
 plt.rc('axes', labelsize=14, titlesize=14)
 plt.rc('legend', fontsize=14)
 plt.rc('xtick', labelsize=10)
+plt.rc('ytick', labelsize=10)
+
+# K-평균
+from sklearn.
