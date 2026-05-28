fe311699dd15210928a55da1b84733cf20701efd
gu-sean <rnfdc4134@gmail.com>
2026-05-29 00:11:16 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -128,6 +128,8 @@ print(dbscan.labels_[:10])
 print(dbscan.core_sample_indices_([:10]))
 print(dbscan.components_)
 
-def plot_dbscan(dbscan,)
+def plot_dbscan(dbscan, X, size, show_xlabel=True,
+                show_ylabel=True):
+    core_m
 
     
