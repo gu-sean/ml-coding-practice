8710ef8b2362ed800d282771f600e557a9eee9b3
gu-sean <rnfdc4134@gmail.com>
2026-05-28 23:55:44 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -69,3 +69,8 @@ print(silhouette_score(X, kmeans.labels_))
 silhouette_scores = [silhouette_score(X, model.labels) for model in kmeans_per_k[1:]]
 
 plt.figure(figsize=(8, 3))
+plt.plot(range(2, 10), silhouette_scores, "bo-")
+plt.xlabel("$k$")
+plt.ylabel("실루엣 스코어")
+plt.title("클러스터 수에 따른 실루엣 스코어")
+plt.show()
