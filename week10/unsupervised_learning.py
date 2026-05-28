e26543015b295f8868ddea0b44a0c48caa5b04a1
gu-sean <rnfdc4134@gmail.com>
2026-05-29 00:13:16 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -132,5 +132,6 @@ def plot_dbscan(dbscan, X, size, show_xlabel=True,
                 show_ylabel=True):
     core_mask = np.zeros_like(dbscan.labels, dtype=bool)
     core_mask[dbscan.core_sample_indices_] = True
-    
+    anomalies_mask = dbscan.labels_ == -1
+    non_core_mask = ~(core_mask | )
     
